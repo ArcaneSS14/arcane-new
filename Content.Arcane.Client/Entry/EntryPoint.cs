774951dd2987148f21da94407ed4f02b7e1cff69
Vecortys <123871947+Vecortys@users.noreply.github.com>
2026-09-16 00:28:30 +0300

    модули

---
+++ Content.Arcane.Client/Entry/EntryPoint.cs
@@ -0,0 +1,5 @@
+using Robust.Shared.ContentPack;
+
+namespace Content.Arcane.Client.Entry;
+
+public sealed class EntryPoint : GameClient;
---
+++ Content.Arcane.Common/Entry/EntryPoint.cs
@@ -0,0 +1,5 @@
+using Robust.Shared.ContentPack;
+
+namespace Content.Arcane.Common.Entry;
+
+public sealed class EntryPoint : GameShared;
